f3b8afbc2cbe7848c5e05747800828d05393a778
Osman Yasir İnan <oyington@gmail.com>
2022-02-26 15:15:55 +0300

    see-more eklendi

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ user.onclick = () => {
         userarrow.classList.add('fa-angle-down');
     }
 }
+
+// const morebtn = document.querySelector('.see-more');
+document.querySelectorAll('.maclar-table').forEach((e) => {
+    console.log(e.id);
+    let more = document.querySelector('#' + e.id + ' .more');
+    let morearrow = document.querySelector('#' + e.id + ' .see-more i');
+
+    morearrow.onclick = () => {
+        more.classList.toggle('active');
+    
+        if (more.classList.contains('active')){
+            morearrow.classList.remove('fa-angle-down');
+            morearrow.classList.add('fa-angle-up')
+        }else {
+            morearrow.classList.remove('fa-angle-up');
+            morearrow.classList.add('fa-angle-down');
+        }
+    }
+});
