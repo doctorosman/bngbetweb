8cf593e3416b236584e7ea70d350577a4798ccf9
Osman Yasir İnan <oyington@gmail.com>
2022-02-28 00:08:50 +0300

    Bakiye yükleme tamamlandı

--- bakiyeyukle/script.js
+++ bakiyeyukle/script.js
@@ -12,3 +12,43 @@ tutar.oninput = () => {
         tutar.value = tutar.value.slice(0,3); 
     }
 }
+
+const odeme_papara = document.querySelector('.odeme-papara');
+const papara_btn = document.querySelector('button#papara');
+const odeme_papara_exit = document.querySelector('.odeme-papara .exit');
+const odeme_papara_iptal = document.querySelector('.odeme-papara button#iptal-et');
+
+papara_btn.onclick = () => {
+    if (parseInt(tutar.value) > 0) {
+        odeme_papara.classList.add('active');
+        
+        document.querySelector('.gtutar span').innerHTML = tutar.value + ',00 ₺';
+    }
+}
+
+const papara_exit = () => {
+    odeme_papara.classList.remove('active');
+}
+
+odeme_papara_exit.onclick = papara_exit;
+odeme_papara_iptal.onclick = papara_exit;
+
+const odeme_elden = document.querySelector('.odeme-elden');
+const elden_btn = document.querySelector('button#elden');
+const elden_exit = document.querySelector('.odeme-elden .exit');
+const elden_iptal = document.querySelector('.odeme-elden button#iptal-et');
+
+const eldenx = () => {
+    odeme_elden.classList.remove('active');
+}
+
+elden_exit.onclick = eldenx;
+elden_iptal.onclick = eldenx;
+
+elden_btn.onclick = () => {
+    if (parseInt(tutar.value) > 0) {
+        odeme_elden.classList.add('active');
+
+        document.querySelector('.ytutar span').innerHTML = tutar.value + ',00 ₺';
+    }
+}
